1e438d6e2820d42a45a5f3f6587626e346d9e18f
Naciye Öngün <naciyeongun@hotmail.com>
2022-05-02 14:50:46 +0300

    update

---
+++ jump game/Assets/Scripts/jump.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour
+{
+    [SerializeField] float jumps = 10f;
+    Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumps);
+        }
+    }
+}
